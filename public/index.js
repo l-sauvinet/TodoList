378924199ec3230777ce0eebf968441c865b63f5
Lucas SAUVINET <l.sauvinet@thomyris.com>
2024-06-14 14:00:58 +0200

    fix: priority task

--- public/index.js
+++ public/index.js
@@ -1,16 +1,20 @@
-const inputBox = document.getElementById("input-box")
-const list = document.getElementById("list-container")
+const inputBox = document.getElementById("input-box");
+const prioritySelect = document.getElementById("priority-select");
+const list = document.getElementById("list-container");
 
 function addTask(){
     if(inputBox.value === ''){
         alert("Vous n'avez écrit aucune tâche");
     } else{
         let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        let priority = prioritySelect.value;
+        li.innerHTML = `${inputBox.value}`;
+        li.setAttribute("data-priority", priority);
         list.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        sortTasks();
     }
     inputBox.value = '';
     saveData();
@@ -32,5 +36,13 @@ function saveData(){
 
 function showListTask(){
     list.innerHTML = localStorage.getItem("data");
+    sortTasks();
 }
+
+function sortTasks() {
+    let items = Array.from(list.children);
+    items.sort((a, b) => a.getAttribute("data-priority") - b.getAttribute("data-priority"));
+    items.forEach(item => list.appendChild(item));
+}
+
 showListTask();
